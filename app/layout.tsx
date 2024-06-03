a53d92c4baa4993410a4bbb4a2d9fca677f22a00
errand <errandvega@gmail.com>
2024-06-03 10:06:11 +0300

    fix: hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      {children}
       <Analytics/>
       <SpeedInsights/>
+      </body>
     </html>
   );
 }
